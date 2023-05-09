3322825a80d45a0ce02ec0bd4c5e0af4c041a04b
sachini nisansala <sachinifonseka08@gmail.com>
2023-05-10 01:30:35 +0530

    invoice added

---
+++ src/components/generateInvoice/InvoiceTableHeader.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Text, View, StyleSheet } from '@react-pdf/renderer';
+
+const borderColor = '#3778C2'
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderRightWidth: 1,
+        borderLeftWidth:1,
+        borderTop:1,
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        height: 24,
+        textAlign: 'center',
+        fontStyle: 'bold',
+        flexGrow: 1,
+    },
+    s: {
+        width: '10%',
+        borderRightWidth:1,
+        
+    },
+    description: {
+        width: '45%',
+        borderRightWidth: 1,
+    },
+    amount: {
+        width: '45%'
+    },
+   
+});
+
+const InvoiceTableHeader = () => (
+    <View style={styles.container}>
+        <Text style={styles.s}>#</Text>
+        <Text style={styles.description}> Description</Text>
+        <Text style={styles.amount}>Amount</Text>
+    </View>
+);
+
+export default InvoiceTableHeader;
---
+++ src/components/generateInvoice/InvoiceTableRow.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+import { Text, View, StyleSheet } from '@react-pdf/renderer';
+
+const borderColor = '#3778C2'
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        height: 24,
+        fontStyle: 'bold',
+        borderRightWidth: 1,
+        borderLeftWidth:1,
+    },
+    s: {
+        width: '10%',
+        borderRightWidth:1,
+        
+    },
+    description: {
+        width: '45%',
+        borderRightWidth: 1,
+       
+    },
+   
+    amount: {
+        width: '45%'
+    },
+});
+
+const InvoiceTableRow = ({ items }) => {
+    const rows = items.map((item,index) =>
+        <View style={styles.row} key={item.sno.toString()}>
+            <Text style={styles.s}>{index}</Text>
+            <Text style={styles.description}>{item.desc}</Text>
+            <Text style={styles.amount}>{item.amount}</Text>
+        </View>
+    );
+    return (<Fragment>{rows}</Fragment>)
+};
+
+export default InvoiceTableRow;
