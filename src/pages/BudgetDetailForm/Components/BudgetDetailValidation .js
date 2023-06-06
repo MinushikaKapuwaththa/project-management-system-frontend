3df88c8d3ac372b82cce2a3d7e8d1d9ec6b82634
sachini nisansala <sachinifonseka08@gmail.com>
2023-06-06 23:25:59 +0530

    basic functions done

--- src/pages/BudgetDetailForm/Components/BudgetDetailValidation .js
+++ src/pages/BudgetDetailForm/Components/BudgetDetailValidation .js
@@ -1,34 +1,33 @@
 export default function validate(values) {
     let errors = {};
-    if (!values.projectName   ) {
-        errors.projectName = 'project Name is required';
+  
+    if (!values.projectName) {
+      errors.projectName = 'Project Name is required';
     }
-    if (!values.projectId  ) {
-        errors.projectId = 'project ID is required';
+  
+    if (!values.projectId) {
+      errors.projectId = 'Project ID is required';
     }
-    if(!values.EstimatedHourlyRate){
-        errors.EstimatedHourlyRate= 'Estimated Hourly Rate  is required';
+  
+    if (!values.EstimatedHourlyRate) {
+      errors.EstimatedHourlyRate = 'Estimated Hourly Rate is required';
+    } 
+    if (!values.cost) {
+      errors.cost = 'Cost by is required';
     }
-    if(!values.cost){
-        errors.cost= 'cost by is required'
-
+  
+    if (!values.record) {
+      errors.record = 'Recorded by is required';
     }
-
-    if(!values.record){
-        errors.record= 'recorded by is required'
-
+  
+    if (!values.price) {
+      errors.price = 'Price is required';
     }
-    if(!values.cost){
-        errors.cost= 'cost by is required'
-
+  
+    if (!values.profit) {
+      errors.profit = 'Profit is required';
     }
-    if(!values.price){
-        errors.price= 'cost by is required'
-
-    }
-    if(!values.profit){
-        errors.profit= 'profit by is required'
-
-    }
+  
     return errors;
   };
+  
--- src/pages/Invoice/Invoice.js
+++ src/pages/Invoice/Invoice.js
@@ -2,62 +2,19 @@ import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 import './Invoice.css';
 import PdfDocument from '../../components/generateInvoice/Invoice';
 
-function Invoice() {
-  const InvoiceData = {
-    id: "5df3180a09ea16dc4b95f910",
-    invoice_no: "201906-28",
-    balance: "$2,283.74",
-    title:"deufe",
-    trans_date: "26-11-2023",
-    address: "Lanka Safety Equipment, \n No 23A,\n Pagoda Road,\n Nugegoda,\nSri Lanka\n\n",
-    client:"Client: Lanka Safety Equipment\n\n",
-    projectName:"Invoice for Website Development & Hosting",
-
-    due_date: "26-11-2021",
-    companyID: "10001",
-    companyName: "xyz company",
+function Invoice({invoiceData}) {
   
-    items: [
-        {
-            sno: 1,
-            desc: "Website Development Cost",
-           
-           
-        },
-        {
-            sno: 2,
-            desc: "Hosting Fee for Year 2019",
-           
-           
-        },
-        {
-            sno: 3,
-            desc: "TOTAL COST",
-           
-        },
-        {
-          sno: 4,
-          desc: "Payment Received (2019/01/08)-ACKG3T11 ",
-         
-      },
-      {
-        sno: 5,
-        desc: "Due Amount",
-       
-    }
-    ]
-}
   const fileName = "Invoice.pdf";
 
   return (
     <div className="App"> 
       <PDFViewer width={800} height={500} showToolbar={false}>
-        <PdfDocument invoicedata={InvoiceData} />
+        <PdfDocument invoicedata={invoiceData} />
       </PDFViewer>
 
       <div className='download-link'>
         <PDFDownloadLink
-          document={<PdfDocument invoicedata={InvoiceData} />}
+          document={<PdfDocument invoicedata={invoiceData} />}
           fileName={fileName}
         >
           {({ blob, url, loading, error }) =>
